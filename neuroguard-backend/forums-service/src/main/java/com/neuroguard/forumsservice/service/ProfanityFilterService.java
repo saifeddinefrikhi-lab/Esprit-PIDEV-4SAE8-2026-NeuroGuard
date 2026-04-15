88c87e46ce8ed5af869ba405d0119a788f151442
jihenammar <jihenammar@gmail.com>
2026-04-15 02:16:12 +0200

    update forums service

--- neuroguard-backend/forums-service/src/main/java/com/neuroguard/forumsservice/service/ProfanityFilterService.java
+++ neuroguard-backend/forums-service/src/main/java/com/neuroguard/forumsservice/service/ProfanityFilterService.java
@@ -1,6 +1,6 @@
 package com.neuroguard.forumsservice.service;
 
-import com.neuroguard.forumsservice.constant.Profanity;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -14,6 +14,24 @@ import java.util.stream.Collectors;
  */
 @Service
 public class ProfanityFilterService {
+
+    @Getter
+    public enum Profanity {
+        DAMN("damn"), HELL("hell"), CRAP("crap"), STUPID("stupid"), IDIOT("idiot"), DUMB("dumb"),
+        SUCK("suck"), SUCKS("sucks"), HATE("hate"), KILL("kill"), DIE("die"), UGLY("ugly"),
+        FAT("fat"), LOSER("loser"), SHUT_UP("shut up"), SHUTUP("shutup"), WTF("wtf"),
+        OMG("omg"), BS("bs"), SCREW("screw"), SCREWED("screwed"), FREAKING("freaking"),
+        FRICKING("fricking"), FRIGGING("frigging"), BLOODY("bloody"), BUGGER("bugger"),
+        ARSE("arse"), ASS("ass"), BITCH("bitch"), BASTARD("bastard"), DICK("dick"),
+        COCK("cock"), PRICK("prick"), PUSSY("pussy"), SLUT("slut"), WHORE("whore"),
+        FAG("fag"), RETARD("retard"), RETARDED("retarded"), NIGGER("nigger"),
+        NIGGA("nigga"), FUCK("fuck"), FUCKING("fucking"), FUCKED("fucked"),
+        FUCKER("fucker"), SHIT("shit"), SHITTY("shitty"), BULLSHIT("bullshit"),
+        DIPSHIT("dipshit"), DIP_STICK("dip stick");
+
+        private final String word;
+        Profanity(String word) { this.word = word; }
+    }
 
     private static final Set<String> BANNED_WORDS = Arrays.stream(Profanity.values())
             .map(Profanity::getWord)
